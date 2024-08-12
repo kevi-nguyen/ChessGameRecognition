daf9a08d60aefa7727354f690e54fb8a017e6c3f
Kevin Nguyen <kevin.nguyen@tum.de>
2024-08-12 18:04:43 +0200

    move image from init parameter to input parameter of functions

--- ChessboardRecognition.py
+++ ChessboardRecognition.py
@@ -3,10 +3,6 @@ import numpy as np
 
 
 class ChessboardRecognition:
-    def __init__(self, image_path):
-        self.image_path = image_path
-        self.image = cv2.imread(self.image_path)
-        self.hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
 
     def cell_to_chess_notation(self, cell):
         # Convert the cell coordinates to chess notation
@@ -28,7 +24,7 @@ class ChessboardRecognition:
         end_row = 8 - int(move_str[3])
         return ((start_row, start_col), (end_row, end_col))
 
-    def transform_image(self):
+    def transform_image(self, image):
         # Define the pixel coordinates of the four corners of the chessboard in the distorted image
         src_points = np.float32([[375, 760], [2530, 726], [325, 2957], [2625, 2936]])
 
@@ -39,7 +35,7 @@ class ChessboardRecognition:
         matrix = cv2.getPerspectiveTransform(src_points, dst_points)
 
         # Apply the perspective transformation to the image
-        corrected_image = cv2.warpPerspective(self.image, matrix, (3024, 4032))
+        corrected_image = cv2.warpPerspective(image, matrix, (3024, 4032))
         return corrected_image
 
     def get_board_state(self, image):
@@ -250,36 +246,3 @@ class ChessboardRecognition:
             board_state[start_row][start_col] = None
 
         return board_state
-
-    def get_board_state_from_stream(self):
-        while True:
-            # Read a frame from the video stream
-            ret, frame = self.video_stream.read()
-
-            # If the frame was read successfully, process it
-            if ret:
-                # Transform the image
-                transformed_image = self.transform_image(frame)
-
-                # Get the board state
-                board_state = self.get_board_state(transformed_image)
-
-                # Output the board state
-                for row in board_state:
-                    print(row)
-
-                # Display the transformed image
-                cv2.imshow('Transformed Image', transformed_image)
-
-                # Break the loop if the 'q' key is pressed
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-            else:
-                break
-
-    def release_stream(self):
-        # When everything done, release the video capture object
-        self.video_stream.release()
-
-        # Closes all the frames
-        cv2.destroyAllWindows()
